e98c002a8fe4e754d77445b80fa2a546ad7452ff
JeanBorba <90288259+JeanBorba@users.noreply.github.com>
2026-08-15 18:32:50 -0300

    Format Terraform stack

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ data "oci_identity_availability_domains" "ads" {
 }
 
 data "oci_core_images" "ubuntu" {
-  compartment_id          = var.compartment_ocid
+  compartment_id           = var.compartment_ocid
   operating_system         = "Canonical Ubuntu"
   operating_system_version = "24.04"
   shape                    = "VM.Standard.A1.Flex"
